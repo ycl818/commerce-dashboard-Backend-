c475f6acc5002926b05687a05a05df47f784199e
yichien <kevinyichienlee@gmail.com>
2023-04-13 00:09:10 +0800

    add new api model and route

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import morgan from "morgan";
 
 import kpiRoutes from "./routes/kpi.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import { kpis, products } from "./data/data.js";
+
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js";
 
 /* configuration */
 
@@ -25,6 +28,7 @@ app.use(cors());
 /* ROUTES
  */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MOONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -36,7 +40,9 @@ mongoose
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
+    //Product.insertMany(products);
   })
   .catch((error) => console.log(`${error} did not connect`));
